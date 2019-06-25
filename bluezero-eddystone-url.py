e78c68b707a724938d8b23806078f290a552805a
raspberrypisig <raspberrypisig@gmail.com>
2019-06-25 18:31:47 +1000

    Update bluezero-eddystone-url.py

--- bluezero-eddystone-url.py
+++ bluezero-eddystone-url.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+# https://bluezero.readthedocs.io/en/latest/system_setup.html
+
 from bluezero import eddystone_beacon
 
 eddystone_beacon.EddystoneURL('https://git.io/fjrcI')
